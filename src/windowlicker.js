81c31f8b808929610de2bc1967bdc4655452b905
Jorn Luiten <js.luiten@gmail.com>
2014-05-26 11:43:43 +0200

    TEMP: added some related material

--- src/windowlicker.js
+++ src/windowlicker.js
@@ -1,2 +1,16 @@
-var matchMedia = require('./matchMedia');
+/**
+ * Default settings:
+ * small <  600
+ * large >= 600
+ *
+ * Set:
+ * windowlicker.set({
+ *   small: [0, 599]
+ * });
+ */
 
+var matchMedia = require('./matchMedia');
+var s = document.head;
+
+
+console.log(s);
